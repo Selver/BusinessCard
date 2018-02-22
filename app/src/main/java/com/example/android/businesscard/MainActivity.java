46f52bce20b6abeed18d717666faf4ca2b083ec7
selver <selverusluserbest@gmail.com>
2018-02-22 22:55:36 +0100

    First commit

---
+++ app/src/main/java/com/example/android/businesscard/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.businesscard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView) findViewById(R.id.join_us_now);
+        img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://eu.udacity.com/"));
+                startActivity(intent);
+            }
+        });
+    }
+
+}
+
